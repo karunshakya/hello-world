9761c4017e8132e80edc089d9959e7349a4b63c6
Karun Shakya <karun.shakya45@gmail.com>
2020-05-30 21:08:43 +0545

    breaking indentations

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 echo "Hello World";
 
 if($_GET['name']){
-  echo "<br>";
-  echo "Hello ".$_GET['name'];
+echo "<br>";
+echo "Hello ".$_GET['name'];
 }
 
 echo $_GET['address'] ? $_GET['address'] : '';
